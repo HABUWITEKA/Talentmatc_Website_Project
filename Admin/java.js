9277ca300e9df63664c04b013bfee76e5825a709
HABUWITEKA <43143832+HABUWITEKA@users.noreply.github.com>
2020-05-12 13:20:03 +0200

    new studd on the way

---
+++ Admin/java.js
@@ -0,0 +1,46 @@
+function section1display() {
+	var section1 = document.getElementById("section1");
+	var section2 = document.getElementById("section2");
+	var section3 = document.getElementById("section3");
+	var section4 = document.getElementById("section4");
+
+	section1.style.display="block";
+	section2.style.display="none";
+	section3.style.display="none";
+	section4.style.display="none";
+
+	console.log(2);
+}
+function section2display() {
+	var section1 = document.getElementById("section1");
+	var section2 = document.getElementById("section2");
+	var section3 = document.getElementById("section3");
+	var section4 = document.getElementById("section4");
+
+	section1.style.display="none";
+	section2.style.display="block";
+	section3.style.display="none";
+	section4.style.display="none";
+}
+function section3display() {
+	var section1 = document.getElementById("section1");
+	var section2 = document.getElementById("section2");
+	var section3 = document.getElementById("section3");
+	var section4 = document.getElementById("section4");
+
+	section1.style.display="none";
+	section2.style.display="none";
+	section3.style.display="block";
+	section4.style.display="none";
+}
+function section4display() {
+	var section1 = document.getElementById("section1");
+	var section2 = document.getElementById("section2");
+	var section3 = document.getElementById("section3");
+	var section4 = document.getElementById("section4");
+
+	section1.style.display="none";
+	section2.style.display="none";
+	section3.style.display="none";
+	section4.style.display="block";
+}
